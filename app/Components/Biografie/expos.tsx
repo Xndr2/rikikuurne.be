c26c146a11ced66d3cbd0a3ac7153c7bee17b8f9
Xndr <moermanxander@gmail.com>
2024-03-26 00:29:24 +0100

    fix for check mark alignment

--- app/Components/Biografie/expos.tsx
+++ app/Components/Biografie/expos.tsx
@@ -19,11 +19,11 @@ export default function Expos(props: ExpoProps) {
     return (
         <ul className="grid grid-cols-2 md:grid-cols-3 gap-x-8 max-w-max m-auto text-left text-gray-500 dark:text-gray-400">
             {expos.map((expo) => (
-                <li key={Math.random()} className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <svg key={Math.random()} className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                <li key="" className="flex items-center">
+                    <svg key="" className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
                         <path stroke="currentColor" d="M1 5.917 5.724 10.5 15 1.5" />
                     </svg>
-                    {expo.place}
+                    <span className="ml-1">{expo.place}</span>
                 </li>
             ))}
         </ul >

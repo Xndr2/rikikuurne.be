aa0d932f6b59bd8e203212def842f9119f8e7e97
Xndr <moermanxander@gmail.com>
2024-03-26 20:17:26 +0100

    keys

--- app/Components/Contact/link.tsx
+++ app/Components/Contact/link.tsx
@@ -15,17 +15,18 @@ interface LinkProps {
 export default function Links(props: LinkProps) {
     const { links } = props;
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:w-max m-auto my-4">
+        <div key="" className="grid grid-cols-1 md:grid-cols-2 gap-4 md:w-max m-auto my-4">
             {links.map((l) => (
-                <Link target='_blank' href={l.link} className="flex items-center text-left border-2 rounded-lg p-2 hover:border-blue-300">
+                <Link key="" target='_blank' href={l.link} className="flex items-center text-left border-2 rounded-lg p-2 hover:border-blue-300">
                     <Image
                         src={l.icon}
                         width={64}
                         height={64}
                         className="h-8 w-8 md:h-10 md:w-10 m-2 invert"
                         alt="unknown"
+                        key=""
                     />
-                    <div className="font-medium ml-2 mr-4 text-xs md:text-sm">
+                    <div key="" className="font-medium ml-2 mr-4 text-xs md:text-sm">
                         <div>{l.name}</div>
                         <div className="text-gray-500 dark:text-gray-400">{l.linkName}</div>
                     </div>

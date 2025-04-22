4264cf25227a4cd6254d641b25f1367e5f7aead0
Xndr <67145274+Xndr2@users.noreply.github.com>
2025-04-22 11:07:26 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         
         {/* Footer */}
         <Footer />
+        <Analytics/>
       </div>
       </body>
     </html>
